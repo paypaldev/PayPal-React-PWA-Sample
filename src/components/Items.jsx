dcb5fa7ddb5872d567121fcb6f5c52efde076b19
Pato Vargas <pevargas2730@gmail.com>
2022-11-01 11:25:17 -0400

    usePayPalScript Reducer

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './Items.css';
 import Item from './Item';
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 
 const Items = () => {
     const [shoes, setShoes] = useState([]);
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+    const [currency, setCurrency] = useState(options.currency);
+
     useEffect(() => {
         const apiURL= "https://store-api-a7c49-default-rtdb.firebaseio.com/shoes.json"
 
@@ -15,11 +18,46 @@ const Items = () => {
             setShoes(data);
         });
     }, []);
+
+    const onCurrencyChange = ({ target: { value } }) => {
+      setCurrency(value);
+      dispatch({
+          type: "resetOptions",
+          value: {
+              ...options,
+              currency: value,
+          },
+      });
+  }
+
+  const onCreateOrder = (data,actions) => {
+      return actions.order.create({
+          purchase_units: [
+              {
+                  amount: {
+                      value: "8.99",
+                  },
+              },
+          ],
+      });
+  }
+
+  const onApproveOrder = (data,actions) => {
+      return actions.order.capture().then((details) => {
+          const name = details.payer.name.given_name;
+          alert(`Transaction completed by ${name}`);
+      });
+  }
+
   if(shoes) {
     return (
         <div className="grid-items">
             {shoes.map(shoe=><Item key={shoe.name} {...shoe} />)}
-            <PayPalButtons style={{ layout: "vertical" }}/>
+            <PayPalButtons 
+              style={{ layout: "vertical" }}
+              createOrder={(data, actions) => onCreateOrder(data, actions)}
+              onApprove={(data, actions) => onApproveOrder(data, actions)}
+            />
         </div>
       );
   } else {
